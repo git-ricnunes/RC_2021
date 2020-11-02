dd7c1de588569467c08c883388cf2906cea43309
RICANUNE <ricardonunes1990@gmail.com>
2020-11-02 17:13:14 +0000

    correcao pd

--- src/PD.c
+++ src/PD.c
@@ -234,11 +234,14 @@ int main(int argc, char *argv[]){
 					if(strcmp(op,"VLC")==0){
   						if(strcmp(userAs,user)==0){                                   
     						strcat(msg,"RVC ");
+							strcat(msg,user);
     						strcat(msg,"OK");
     						strcat(msg,"\n");
     						
    					  } else {	
     						strcat(msg,"RVC ");
+							strcat(msg,user);
+    						strcat(msg," ");
     						strcat(msg,"NOK");
     						strcat(msg,"\n");
   						}
