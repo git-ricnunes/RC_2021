c595a0f9e22a7b7e42930e6228af6d465c739374
b1sharp <iredzmv@gmail.com>
2020-11-02 11:27:05 -0100

    read/write + alguns comandos fs

--- src/User.c
+++ src/User.c
@@ -1,74 +1,167 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+
 #include <sys/select.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+
+#include <errno.h>
+#include <time.h>
+
 #define DEFAULT_ASIP "127.0.0.1"
 #define DEFAULT_ASPORT "58011"
 #define DEFAULT_FSIP "127.0.0.1"
 #define DEFAULT_FSPORT "59011"
+
 #define IP_SIZE 64
 #define PORT_SIZE 16
 #define BUFFER_SIZE 128
-#define UID_SIZE 32
-#define PASS_SIZE 32
 #define MAX_TOKENS 4
+#define CODE_SIZE 4
+#define UID_SIZE 64
+#define PASS_SIZE 64
+#define RID_SIZE 5
+#define TID_SIZE 5
+//#define STATUS_SIZE 6
+//#define LOGGED_IN 1
+//#define LOGGED_OUT 0
+#define AS_FD_SET 1
+#define FS_FD_SET 2
 
-int fd, fdAS, fdFS, errcode;
+int fdAS, fdFS, errcode;
 ssize_t n;
 socklen_t addrlenAS, addrlenFD;
 struct addrinfo hintsAS, *resAS, hintsFD, *resFD;
 struct sockaddr_in addrAS, addrFD;
+char buffer[BUFFER_SIZE];
+char msg[BUFFER_SIZE];
+
+void write_msg(int fd){
+
+	int n_sum = 0;
+	int n_msg = strlen(msg);
+	int n_sent;
+
+	while(1){
+		if (fd == AS_FD_SET)
+			n_sent = write(fdAS, msg, strlen(msg));
+		else if (fd == FS_FD_SET)
+			n_sent = write(fdFS, msg, strlen(msg));
+
+		if (n_sent == -1){ /* Err */
+			fprintf(stderr, "Error: failed to send ""%s"" to authentication server\n", msg);
+			fprintf(stderr, "Error code: %d\n", errno);
+			exit(1);
+		}
+
+		n_sum += n_sent;
+		if (n_sum == n_msg) /* All bytes written */
+			return;
+		else /* Still bytes left to write */
+			continue;
+	}
+}
+
+int read_msg(int fd){
+
+	int n_sum = 0;
+	int n_rec;
+
+	while(1){
+		if (fd == AS_FD_SET)
+			n_rec = read(fdAS, buffer, sizeof(buffer));
+		else if (fd == FS_FD_SET)
+			n_rec = read(fdFS, buffer, sizeof(buffer));
+
+		if (n_rec == -1){ /* Err */
+			fprintf(stderr, "Error: failed to read message from authentication server\n");
+			fprintf(stderr, "Error code: %d\n", errno);
+			exit(1);
+		}
+
+		n_sum += n_rec;
+		if (buffer[n_sum-1] == '\n') /* All bytes read */
+			return n_sum;
+		else /* Still bytes left to read */
+			continue;
+	}
+}
 
 int main(int argc, char *argv[]){
 
-	char ipAS[IP_SIZE] = DEFAULT_ASIP, portAS[PORT_SIZE] = DEFAULT_ASPORT, ipFS[IP_SIZE] = DEFAULT_FSIP, portFS[PORT_SIZE] = DEFAULT_FSPORT;
+	char ipAS[IP_SIZE] = DEFAULT_ASIP;
+	char portAS[PORT_SIZE] = DEFAULT_ASPORT;
+	char ipFS[IP_SIZE] = DEFAULT_FSIP;
+	char portFS[PORT_SIZE] = DEFAULT_FSPORT;
 
-	for (int i = 1; i < argc; i++){
+	for (int i = 1; i < argc - 1; i++){
 		if (argv[i][0] != '-' || strlen(argv[i]) != 2) continue;
 		switch (argv[i][1]){
 			case 'n':
-				strcpy(ipAS, argv[i+1]);
+				if (argv[i+1][0] != '-' || strlen(argv[i+1]) != 2)
+					strcpy(ipAS, argv[i+1]);
 				break;
 			case 'p':
-				strcpy(portAS, argv[i+1]);
+				if (argv[i+1][0] != '-' || strlen(argv[i+1]) != 2)
+					strcpy(portAS, argv[i+1]);
 				break;
 			case 'm':
-				strcpy(ipFS, argv[i+1]);
+				if (argv[i+1][0] != '-' || strlen(argv[i+1]) != 2)
+					strcpy(ipFS, argv[i+1]);
 				break;
 			case 'q':
-				strcpy(portFS, argv[i+1]);
+				if (argv[i+1][0] != '-' || strlen(argv[i+1]) != 2)
+					strcpy(portFS, argv[i+1]);
 				break;
-			default: /* invalid flag */
-				continue;
+			default:
+				fprintf(stderr, "Invalid flag\n");
+				fprintf(stderr, "Usage: ./user [-n ASIP] [-p ASport] [-m FSIP] [-q FSPort]\n");
+				exit(1);
 		}
 	}
 
-	fdAS=socket(AF_INET,SOCK_STREAM,0);
-	if(fdAS==-1) exit(1);
+	fdAS = socket(AF_INET, SOCK_STREAM, 0);
+	if (fdAS == -1){
+		fprintf(stderr, "Error: unable to create tcp client socket\n");
+		fprintf(stderr, "Error code: %d\n", errno);
+	}
 		
-	memset(&hintsAS,0,sizeof hintsAS);
-	hintsAS.ai_family=AF_INET;
-	hintsAS.ai_socktype=SOCK_STREAM;
+	memset(&hintsAS, 0, sizeof hintsAS);
+	hintsAS.ai_family = AF_INET;
+	hintsAS.ai_socktype = SOCK_STREAM;
 		
-	errcode = getaddrinfo(ipAS,portAS,&hintsAS,&resAS);
-	if(errcode!=0) exit(1);
+	errcode = getaddrinfo(ipAS, portAS, &hintsAS, &resAS);
+	if (errcode != 0){
+		fprintf(stderr, "Error: unable to get authentication server address info\n");
+		fprintf(stderr, "Error code: %d\n", errno);
+	}
 
-	n=connect(fdAS,resAS->ai_addr,resAS->ai_addrlen);
-	if(n==-1) exit(1);
+	n = connect(fdAS, resAS->ai_addr, resAS->ai_addrlen);
+	if(n == -1){
+		fprintf(stderr, "Error: failed to connect to authentication server\n");
+		fprintf(stderr, "Error code: %d\n", errno);
+	}
 
-	char buffer[BUFFER_SIZE];
+	printf("Connected to Authentication Server ""%s"" in port %s\n", ipAS, portAS);
+
+	char code[CODE_SIZE] = "";
 	char uid[UID_SIZE] = "";
 	char pass[PASS_SIZE] = "";
-	int rid = 9999; // rng/list?
+	char rid[RID_SIZE] = "";
+	char tid[TID_SIZE] = "";
+	//char status[STATUS_SIZE] = "";
+	//int session = LOGGED_OUT;
+
+	srand(time(NULL));
 
 	while(1){
 
+		printf("Enter a command: ");
 		fgets(buffer, BUFFER_SIZE, stdin);
 
 		int num_tokens = 0;
@@ -80,33 +173,56 @@ int main(int argc, char *argv[]){
 			token = strtok(NULL, " \n");
 		}
 
-		char msg[BUFFER_SIZE] = "";
-		if (!strcmp(token_list[0], "login") && num_tokens == 3){
-			sprintf(msg, "LOG %s %s\n", token_list[1], token_list[2]);
-			fd = fdAS;
+		int fd = 0;
+
+		if (!strcmp(token_list[0], "login") && num_tokens == 3){ /* LOG UID pass */
+			sprintf(code, "LOG");
+			strcpy(uid, token_list[1]);
+			strcpy(pass, token_list[2]);
+			sprintf(code, "%s %s %s\n", code, uid, pass);
+			fd = AS_FD_SET;
 		}
-		else if (!strcmp(token_list[0], "req") && num_tokens == 3){ //RID rng?
-			sprintf(msg, "REQ %s %d %s %s\n", uid, rid, token_list[1], token_list[2]);
-			fd = fdAS;
+		else if (!strcmp(token_list[0], "req")){ /* REQ UID RID Fop [Fname] */
+			sprintf(code, "REQ");
+			sprintf(rid, "%d%d%d%d", rand()%9, rand()%9, rand()%9, rand()%9);
+			if ((!strcmp(token_list[1], "L") || !strcmp(token_list[1], "X")) && num_tokens == 2){
+				sprintf(msg, "%s %s %s %s\n", code, uid, rid, token_list[1]);
+				fd = AS_FD_SET;
+			}
+			else if ((!strcmp(token_list[1], "R") || !strcmp(token_list[1], "U") || !strcmp(token_list[1], "D")) && num_tokens == 3){
+				sprintf(msg, "%s %s %s %s %s\n", code, uid, rid, token_list[1], token_list[2]);
+				fd = AS_FD_SET;
+			}
 		}
-		else if (!strcmp(token_list[0], "val") && num_tokens == 2){
-			sprintf(msg, "AUT %s %d %s\n", uid, rid, token_list[1]);
-			fd = fdAS;
+		else if (!strcmp(token_list[0], "val") && num_tokens == 2){ /* AUT UID RID VC */
+			sprintf(code, "AUT");
+			sprintf(msg, "%s %s %s %s\n", code, uid, rid, token_list[1]);
+			fd = AS_FD_SET;
 		}
-		else if ((!strcmp(token_list[0], "list") || !strcmp(token_list[0], "l")) && num_tokens == 1){
-			fd = fdFS;
+		else if ((!strcmp(token_list[0], "list") || !strcmp(token_list[0], "l")) && num_tokens == 1){ /* LST UID TID */
+			sprintf(code, "LST");
+			sprintf(msg, "%s %s %s\n", code, uid, tid);
+			fd = FS_FD_SET;
 		}
-		else if ((!strcmp(token_list[0], "retrieve") || !strcmp(token_list[0], "r")) && num_tokens == 2){
-			fd = fdFS;
+		else if ((!strcmp(token_list[0], "retrieve") || !strcmp(token_list[0], "r")) && num_tokens == 2){ /* RTV UID TID Fname */
+			sprintf(code, "RTV");
+			sprintf(msg, "%s %s %s %s\n", code, uid, tid, token_list[1]); /* inc: char Fname */
+			fd = FS_FD_SET;
 		}
-		else if ((!strcmp(token_list[0], "upload") || !strcmp(token_list[0], "u")) && num_tokens == 2){
-			fd = fdFS;
+		else if ((!strcmp(token_list[0], "upload") || !strcmp(token_list[0], "u")) && num_tokens == 2){ /* UPL UID TID Fname Fsize data */
+			sprintf(code, "UPL");
+			/* inc */
+			fd = FS_FD_SET;
 		}
-		else if ((!strcmp(token_list[0], "delete") || !strcmp(token_list[0], "d")) && num_tokens == 2){
-			fd = fdFS;
+		else if ((!strcmp(token_list[0], "delete") || !strcmp(token_list[0], "d")) && num_tokens == 2){ /* DEL UID TID Fname */
+			sprintf(code, "DEL");
+			sprintf(msg, "%s %s %s %s\n", code, uid, tid, token_list[1]); /* inc: char Fname */
+			fd = FS_FD_SET;
 		}
-		else if ((!strcmp(token_list[0], "remove") || !strcmp(token_list[0], "x")) && num_tokens == 1){
-			fd = fdFS;
+		else if ((!strcmp(token_list[0], "remove") || !strcmp(token_list[0], "x")) && num_tokens == 1){ /* REM UID TID */
+			sprintf(code, "REM");
+			sprintf(msg, "%s %s %s\n", code, uid, tid);
+			fd = FS_FD_SET;
 		}
 		else if (!strcmp(token_list[0], "exit") && num_tokens == 1){
 			freeaddrinfo(resAS);
@@ -114,17 +230,30 @@ int main(int argc, char *argv[]){
 			break;
 		}
 		else{
-			continue; //invalid command (?)
+			fprintf(stderr, "Invalid command\n");
+			exit(1);
 		}
 		
-		n=write(fd,msg,strlen(msg));
-		if(n==-1) exit(1);
+		if (fd == AS_FD_SET){
+
+			write_msg(AS_FD_SET);
 		
-		n=read(fd,buffer,sizeof(buffer));
-		if(n==-1) exit(1);
-		
-		write(1,"echo: ",6); write(1,buffer,n);
-		//if rlo ok >> guardar uid e pass
+			n = read_msg(AS_FD_SET);
+
+			write(1, "echo: ", 6); write(1, buffer, n);
+
+			if (!strcmp(code, "AUT")){
+				//sscanf tid
+			}
+
+		}
+		else if (fd == FS_FD_SET){
+			//new tcp, incomplete fs interaction
+		}
+
+		memset(buffer, 0, BUFFER_SIZE);
+		memset(msg, 0, BUFFER_SIZE);
+		memset(code, 0, CODE_SIZE);
 	}
 	return 0;
 }
