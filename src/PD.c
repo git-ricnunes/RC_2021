edd819e1d745ac9a74c62b98603fa29999fa77c0
RICANUNE <ricardonunes1990@gmail.com>
2020-11-02 17:16:13 +0000

    correcao no pd

--- src/PD.c
+++ src/PD.c
@@ -235,6 +235,7 @@ int main(int argc, char *argv[]){
   						if(strcmp(userAs,user)==0){                                   
     						strcat(msg,"RVC ");
 							strcat(msg,user);
+							strcat(msg," ");
     						strcat(msg,"OK");
     						strcat(msg,"\n");
     						
