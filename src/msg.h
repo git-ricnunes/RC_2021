20e63fa852a91862df8a92eb1eeebc69413ed741
b1sharp <iredzmv@gmail.com>
2020-11-11 21:19:02 -0100

    includes

--- src/msg.h
+++ src/msg.h
@@ -1,10 +1,11 @@
 #ifndef MSG_H_
 #define MSG_H_
 
-#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 void write_buf(int fd, char* buf);
 int write_buf_SIGPIPE(int fd, char* buf);
