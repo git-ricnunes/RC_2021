e77d1649ee0c421706d77a80866cd4b1626372f8
b1sharp <iredzmv@gmail.com>
2020-11-12 04:24:05 -0100

    recv_file

--- src/tcpFiles.c
+++ src/tcpFiles.c
@@ -6,7 +6,7 @@ void send_file(int fd, FILE * fp, int fsize, char * buffer, int buffer_size){
 	while(1){
 		memset(buffer, 0, buffer_size);
 		n_read = fread(buffer, 1, buffer_size, fp);
-		if (ferror(fp)){
+		if (ferror(fp)){ /* Err */
 			fprintf(stderr, "Error reading file\n");
 			fprintf(stderr, "Error code: %d\n", errno);
 			exit(1);
@@ -24,6 +24,30 @@ void send_file(int fd, FILE * fp, int fsize, char * buffer, int buffer_size){
 	}
 }
 
-void recv_file(int fd, char * Fname, int fsize, char * buffer, int buffer_size){
-	
+//add send_file_sigpipe
+
+void recv_file(int fd, FILE * fp, int fsize, char * buffer, int buffer_size){
+	int n_sum = 0;
+	int n_read;
+	int n_wrtn;
+	while(1){
+		if (strlen(buffer) > 0){ /* Write initial data to file if there is any */
+			n_wrtn = fwrite(buffer, 1, strlen(buffer), fp); //fwrite = write? (write all characters read)
+			if (n_wrtn == -1) { /* Err */
+            	fprintf(stderr, "Error: failed to write data to file\n");
+            	fprintf(stderr, "Error code: %d\n", errno);
+            	exit(1);
+        	}
+        	n_sum += n_wrtn;
+		}
+		if ((fsize == (n_sum - 1)) && (buffer[strlen(buffer) - 1] == '\n'))
+			return;
+		memset(buffer, 0, buffer_size);
+		n_read = read(fd, buffer, buffer_size);
+		if (n_read == -1) { /* Err */
+            fprintf(stderr, "Error: failed to read data\n");
+            fprintf(stderr, "Error code: %d\n", errno);
+            exit(1);
+        }
+	}
 }
