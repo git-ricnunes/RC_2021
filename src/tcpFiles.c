7fa0dca271ad8a8e4a76cd559ee8e74bd644f53a
b1sharp <iredzmv@gmail.com>
2020-11-12 19:57:18 -0100

    send_file fixed

--- src/tcpFiles.c
+++ src/tcpFiles.c
@@ -1,30 +1,74 @@
 #include "tcpFiles.h"
 
-void send_file(int fd, FILE * fp, int fsize, char * buffer, int buffer_size){
+void send_file(int fd, char *fname, int sp){
 	int n_sum = 0;
 	int n_read;
+	int n_sp;
+	FILE * fp = fopen(fname, "r");
+	if (!fp){
+		fprintf(stderr, "Error: failed to open file ""%s""\n", fname);
+		fprintf(stderr, "Error code: %d\n", errno);
+		exit(1);
+	}
+	if (fseek(fp, 0L, SEEK_END) == -1){
+		fprintf(stderr, "Error: failed to positioning file ""%s"" indicator\n", fname);
+		fprintf(stderr, "Error code: %d\n", errno);
+		exit(1);
+	}
+	fsize = ftell(fp);
+	if (fsize == -1){
+		fprintf(stderr, "Error: failed to read file ""%s"" size\n", fname);
+		fprintf(stderr, "Error code: %d\n", errno);
+		exit(1);
+	}
+	else if (fsize >= 10000000000){
+		fprintf(stderr, "Error: file ""%s"" size too big\n", fname);
+		exit(1);
+	}
+	memset(data, 0, DATA_SIZE);
+	sprintf(data, "%d ", fsize);
+	if (sp == SP_CHECK){
+		n_sp = write_buf_SIGPIPE(fd, data);
+		if (n_sp != 0){
+			fclose(fp);
+			return;
+		}
+	}
+	else
+		write_buf(fd, data);
+	fseek(fp, 0L, SEEK_SET);
 	while(1){
-		memset(buffer, 0, buffer_size);
-		n_read = fread(buffer, 1, buffer_size, fp);
+		memset(data, 0, DATA_SIZE);
+		n_read = fread(data, 1, DATA_SIZE, fp);
 		if (ferror(fp)){ /* Err */
 			fprintf(stderr, "Error reading file\n");
 			fprintf(stderr, "Error code: %d\n", errno);
 			exit(1);
 		}
 		if (n_read > 0){
-			write_buf(fd, buffer);
+			if (sp == SP_CHECK){
+				n_sp = write_buf_SIGPIPE(fd, data);
+				if (n_sp != 0){
+					fclose(fp);
+					return;
+				}
+			}
+			else
+				write_buf(fd, data);
 			n_sum += n_read;
 		}
 		if (n_sum == fsize){
-			memset(buffer, 0, buffer_size);
-			sprintf(buffer, "\n");
-			write_buf(fd, buffer);
+			memset(data, 0, DATA_SIZE);
+			sprintf(data, "\n");
+			if (sp == SP_CHECK)
+				write_buf_SIGPIPE(fd, data);
+			else
+				write_buf(fd, data);
+			fclose(fp);
 			return;
 		}
 	}
 }
-
-//add send_file_sigpipe
 
 void recv_file(int fd, FILE * fp, int fsize, char * buffer, int buffer_size){
 	int n_sum = 0;
